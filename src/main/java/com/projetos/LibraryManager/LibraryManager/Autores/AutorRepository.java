bc8e0bc9a306c1e6c37ccfbc69b5783606656145
igorpandolf <pandolf.igor@gmail.com>
2025-09-10 21:53:57 -0300

    feat: cria AutorRepository e LivroRepository extendendo JpaRepository

---
+++ src/main/java/com/projetos/LibraryManager/LibraryManager/Autores/AutorRepository.java
@@ -0,0 +1,6 @@
+package com.projetos.LibraryManager.LibraryManager.Autores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<AutorModel, Long> {
+}
